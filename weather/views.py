22c1209ac210f71eda395c392a765f9583931d03
Carmel Adikin <carmeladikin@gmail.com>
2024-03-31 11:51:27 +0100

    write the complete logic of index page

--- weather/views.py
+++ weather/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 import requests
+from .models import City
 
-def index(request):
+def get_city_weather(city):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=ae3be71be8d31a5c9468dad29db0653e'
-    city = 'Los Angeles'
     response = requests.get(url.format(city)).json()
     city_weather = {
         'city': city,
@@ -11,5 +11,16 @@ def index(request):
         'description': response['weather'][0]['description'],
         'icon': response['weather'][0]['icon'],
     }
+    return city_weather
 
-    return render(request, 'weather/index.html')
+def index(request):
+    weather_data = []
+    cities = City.objects.all()
+
+    for city in cities:
+        city_weather = get_city_weather(city)
+        weather_data.append(city_weather) 
+    
+    context = {'weather_data' : weather_data}
+
+    return render(request, 'weather/index.html', context)
