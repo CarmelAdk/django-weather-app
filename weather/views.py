1d3cfacfcbedc20988505b09504e5ee4651c8543
Carmel Adikin <carmeladikin@gmail.com>
2024-03-31 11:35:14 +0100

    add model for City and register it in admin dashboard

--- weather/views.py
+++ weather/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+import requests
 
 def index(request):
-    context = {
-        'city'
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=ae3be71be8d31a5c9468dad29db0653e'
+    city = 'Los Angeles'
+    response = requests.get(url.format(city)).json()
+    city_weather = {
+        'city': city,
+        'temperature': response['main']['temp'],
+        'description': response['weather'][0]['description'],
+        'icon': response['weather'][0]['icon'],
     }
+
     return render(request, 'weather/index.html')
